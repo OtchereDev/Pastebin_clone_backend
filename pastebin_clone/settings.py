a5d3c418249b61b6d6232a9018323b13cf8e536e
Oliver Otchere <Oliverotchere4@gmail.com>
2022-01-02 21:33:44 +0000

    setup media and static files

--- pastebin_clone/settings.py
+++ pastebin_clone/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
+
+MEDIA_ROOT = "media"
+STATIC_ROOT = "staticfiles"
 
 
 # Restframework settings
@@ -161,3 +169,7 @@ CORS_ALLOW_ALL_ORIGINS=True
 
 # Frontend Setting
 FRONTEND_HOST='http://localhost:8080'
+
+DEFAULT_AUTO_FIELD="django.db.models.BigAutoField"
+
+BASE_URL='http://localhost:8000'
